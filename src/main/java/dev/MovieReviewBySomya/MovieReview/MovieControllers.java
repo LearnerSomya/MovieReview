600b658c5ae6e668705647b4c0043b6665e97507
LearnerSomya <somyasinghal71001@gmail.com>
2023-12-28 23:16:08 +0530

    Backend

--- src/main/java/dev/MovieReviewBySomya/MovieReview/MovieControllers.java
+++ src/main/java/dev/MovieReviewBySomya/MovieReview/MovieControllers.java
@@ -4,11 +4,12 @@ package dev.MovieReviewBySomya.MovieReview;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 //here in RequestMapping("/api/v1/movies") it means any repuest made to the api which is ending with "/api/v1/movies" 
@@ -18,8 +19,9 @@ public class MovieControllers {
     @Autowired
     private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> getAllMovies(){
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        //this "allMovies" are in MovieService.java
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
 }
